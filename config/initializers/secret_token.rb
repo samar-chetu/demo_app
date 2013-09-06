d0aed00d769e971d7f1d8a55790b56396fc5bb41
root <root@uniguestror-VirtualBox.(none)>
2013-09-06 18:12:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '53312f8570cae24496697360af38e930a00b41bb4df74834f89cd342f504951ae808cea2db96de2813efcaa17c2c963c2023956fd6166e510a8c6e76e1fceec8'
